30800b271d504f3f1b8d2191f4dab86fa29bdd54
Kirill Yusov <kyusov@gmail.com>
2024-04-06 15:33:39 +0800

    Правки

--- backend/controllers/Comment.controller.js
+++ backend/controllers/Comment.controller.js
@@ -3,6 +3,14 @@ const { Comment } = require('../models')
 module.exports = {
     async create(req, res) {
         const { text, user_id, objectId, type } = req.body
+
+        /**
+         * 1. консистентные имена "user_id, object_id || userId, objectId"\
+         * 2. добавить поле time в req.body, т.к. запрос может прийти с задержкой и время будет неверное
+         * 3. добавить в req.body поля record_id && post_id, в зависимости от того где оставили комментарий
+         *    одно поле указать null, а второе заполнить значением
+         */
+
         let [record_id, post_id] = [null, null]
         let datetime = Date.now()
         switch (type) {
@@ -31,7 +39,7 @@ module.exports = {
 
     async getBy(req, res) {
         const { objectid, type } = req.query
-        console.log(objectid, type)
+        console.log(objectid, type) // перед коммитом убираем
         let [record_id, post_id] = [null, null]
         switch (type) {
             case 'record':
